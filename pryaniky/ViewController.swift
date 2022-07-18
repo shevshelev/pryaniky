77401afc14e519c925eb60be9eda001168c1b87b
Shevshelev Lev <shevshelev.lev@gmail.com>
2022-07-18 18:06:24 +0500

    Initial Commit

---
+++ pryaniky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pryaniky
+//
+//  Created by Shevshelev Lev on 18.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
